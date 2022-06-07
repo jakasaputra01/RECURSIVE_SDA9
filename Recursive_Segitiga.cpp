4e4a8db4f2b63a9620eb7b4399c61d0f1337061a
jakasaputra01 <105882316+jakasaputra01@users.noreply.github.com>
2022-06-07 17:46:51 +0700

    Create Recursive_Segitiga.cpp

---
+++ Recursive_Segitiga.cpp
@@ -0,0 +1,30 @@
+//Nama  : Jaka Saputra
+//NPM   : G1A016020
+//Kelas : InformatikaB
+
+#include <iostream>
+using namespace std;
+
+void printn(int jumlah)
+{
+    if (jumlah == 0)
+        return;
+    cout << "* ";
+    printn(jumlah - 1);
+}
+ 
+void segitiga(int n, int i){
+    if (n == 0)
+        return;
+    printn(i);
+    cout << endl;
+    segitiga(n - 1, i + 1);
+}
+int main()
+{
+    int n;
+    printf("masukkan jumlah segitiga : ");
+    cin>>n;
+    segitiga(n, 1);
+    return 0;
+}
